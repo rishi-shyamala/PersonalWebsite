33055e9d06c3a6bc09c2915e35b3a2aa4220f511
Rishi Shyamala <rishi@x1-extreme.lan>
2020-10-02 14:54:23 -0400

    restructure folders

---
+++ Projects/index.js
@@ -0,0 +1,19 @@
+'user strict';
+
+const express = require('express');
+
+//constants
+const PORT = 443;
+const HOST = '127.0.0.1';
+
+//App
+const app = express();
+app.get('/', (req, res) => {
+	res.send('Hello world!');
+});
+
+//main
+app.listen(PORT, HOST);
+console.log('Running on https://${HOST}:${PORT}');
+
+
